e7adda4a113d3d84d2168d519e52edcc6d0d4d50
Adam Price <adam@cadno.io>
2022-01-21 09:44:06 -0500

    Initial Commit

---
+++ Swordle/SwordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwordleApp.swift
+//  Swordle
+//
+//  Created by Adam Price on 21/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
